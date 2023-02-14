add78cf92983bad6f895f906959a6ffc82dbf49a
Hyeonwoo Kang <kanglib14@gmail.com>
2023-02-14 14:20:33 +0900

    Update safety.js

--- safety.js
+++ safety.js
@@ -1,9 +1,14 @@
+// ==UserScript==
+// ==/UserScript==
+
 setInterval(function() {
     var node = document.querySelector('.btn_WF_VpPlay');
     node.focus();
+
     var clickEvent = document.createEvent('MouseEvents');
     clickEvent.initEvent('mousedown', true, true);
     node.dispatchEvent(clickEvent);
+
     var clickEvent = document.createEvent('MouseEvents');
     clickEvent.initEvent('mouseup', true, true);
     node.dispatchEvent(clickEvent);
